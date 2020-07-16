4638f410df68dee347356336a7a186b3cd52fd87
Rodrigo-PcGamer <el_stroke_666@hotmail.com>
2020-07-16 10:36:47 -0500

    ultimo commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
+const proyectosController = require('../controllers/proyectosController');
 module.exports = function () {
   //ruta para el home
-  router.use("/", (req, res) => {
-    res.send('index');
-  });
+  router.use("/", proyectosController.proyectosHome);
   router.use("/", (req, res) => {
     res.send('Nosotros');
   });
